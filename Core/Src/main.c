9c7811db0e4afaf27a273279c2e60c1e3c053a51
Konrad <kobielus63@gmail.com>
2026-02-19 08:19:51 +0100

    pin configuration

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "opamp.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -86,6 +87,8 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_OPAMP1_Init();
+  MX_OPAMP2_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
