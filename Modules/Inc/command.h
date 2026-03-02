e87f250b0d56e0069cf95f691a97816ccfb9ad30
Konrad <kobielus63@gmail.com>
2026-03-02 18:27:22 +0100

    command class implementation

---
+++ Modules/Inc/command.h
@@ -0,0 +1,66 @@
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <optional>
+#include <type_traits>
+#include <utility>
+
+#pragma once
+
+class command_t
+{
+
+    const char *CMD_PREFIX     = "cmd";
+    const char *HEX_PREFIX     = "0x";
+    const char *BINARY_PREFIX  = "0b";
+    const char *OCTET_PREFIX   = "0";
+    const char *DECIMAL_PREFIX = "";
+
+  public:
+    // This enum represent most common and frequenty used layout of command parameters. There is no obstacle to use
+    // parameters in different purpose
+    enum class default_command_layout_t : uint8_t
+    {
+        COMMAND_ID,
+        INTERFACE,
+        BRIDGE_1,
+        BRIDGE_2,
+        BRIDGE_3,
+        BRIDGE_4,
+        EXTRA_PARAM,
+
+        // must be last
+        PARAMETERS_COUNT,
+    };
+
+    uint8_t parameters[static_cast<uint8_t>(default_command_layout_t::PARAMETERS_COUNT)];
+
+    using parameters_type = typename std::remove_reference_t<decltype(parameters[0])>;
+
+    enum class command_code_t : parameters_type
+    {
+        SCHEDULE_MEASUREMENT,
+        EXECUTE_MEASUREMENTS,
+        REMOVE_MEASUREMENT,
+        REMOVE_ALL_MEASUREMENTS,
+        SERIALIZE_COMMANDS,
+        RESTORE_SERIALIZED_COMMANDS,
+        REMOVE_SERIALIZED_COMMANDS,
+    };
+
+    // dummy static assert, just to learn something and enjoy coding
+    static_assert(
+        std::is_same_v<std::remove_reference_t<decltype(parameters[0])>, std::underlying_type_t<command_code_t>>,
+        "Typ elementu parameters i enum command_code_t nie zgadza się!");
+
+    static_assert(std::is_same_v<std::remove_reference_t<decltype(parameters[0])>,
+                                 std::underlying_type_t<decltype(command_code_t::SCHEDULE_MEASUREMENT)>>,
+                  "Typ elementu parameters i enum command_code_t nie zgadza się!");
+
+    command_t() { memset(parameters, 0, sizeof(parameters)); }
+    command_t(const command_t &command) { memcpy(parameters, command.parameters, sizeof(parameters)); }
+
+    uint8_t read_numberf_from_string(char *&ptr);
+
+    bool decode_frame(std::optional<std::pair<uint8_t *, uint8_t>> frame);
+};
---
+++ Modules/Src/command.cpp
@@ -0,0 +1,82 @@
+#include "command.h"
+
+uint8_t command_t::read_numberf_from_string(char *&ptr)
+{
+    if (memcmp(ptr, HEX_PREFIX, strlen(HEX_PREFIX)) == 0)
+    {
+        ptr += strlen(HEX_PREFIX);
+        return static_cast<uint8_t>(strtoul(ptr, &ptr, 16));
+    }
+    else if (memcmp(ptr, BINARY_PREFIX, strlen(BINARY_PREFIX)) == 0)
+    {
+        ptr += strlen(BINARY_PREFIX);
+        return static_cast<uint8_t>(strtoul(ptr, &ptr, 2));
+    }
+    else if (memcmp(ptr, OCTET_PREFIX, strlen(OCTET_PREFIX)) == 0)
+    {
+        ptr += strlen(OCTET_PREFIX);
+        return static_cast<uint8_t>(strtoul(ptr, &ptr, 8));
+    }
+    else if (memcmp(ptr, DECIMAL_PREFIX, strlen(DECIMAL_PREFIX)) == 0)
+    {
+        ptr += strlen(DECIMAL_PREFIX);
+        return static_cast<uint8_t>(strtoul(ptr, &ptr, 10));
+    }
+
+    return 0;
+}
+
+bool command_t::decode_frame(std::optional<std::pair<uint8_t *, uint8_t>> frame)
+{
+    if (!frame.has_value())
+    {
+        return false;
+    }
+
+    const char *base_ptr = reinterpret_cast<const char *>(frame.value().first);
+    char *frame_content = reinterpret_cast<char *>(frame.value().first);
+    uint8_t frame_size = frame.value().second;
+
+    if (frame_content == nullptr)
+    {
+        return false;
+    }
+
+    if (frame_size < strlen(CMD_PREFIX))
+    {
+        return false;
+    }
+
+    if (memcmp(frame_content, CMD_PREFIX, strlen(CMD_PREFIX)) != 0)
+    {
+        return false;
+    }
+    frame_content += strlen(CMD_PREFIX);
+    frame_content += strlen(" ");
+
+    static_assert(std::is_same_v<std::remove_reference_t<decltype(parameters[0])>, uint8_t>,
+                  "niezgodne typy parametrów komend");
+    for (uint8_t i = 0; i < sizeof(parameters) && frame_content != nullptr && frame_content - base_ptr <= frame_size;
+         i++)
+    {
+        parameters[i] = read_numberf_from_string(frame_content);
+
+        if (frame_content == nullptr)
+        {
+            return false;
+        }
+
+        if (*frame_content == '\0')
+        {
+            return true;
+        }
+
+        if (*frame_content != ' ')
+        {
+            return false;
+        }
+
+        frame_content += strlen(" ");
+    }
+    return true;
+}
