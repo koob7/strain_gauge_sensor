096524e0d1db3145618bec784795d086f52dad06
Konrad <kobielus63@gmail.com>
2026-03-02 18:27:44 +0100

    add _ms macro

--- Modules/Inc/types.h
+++ Modules/Inc/types.h
@@ -3,10 +3,15 @@
 
 #pragma once
 
+#define _ms *1000
+#define _s *1000
+#define _min *60_s
+#define _h *60_min
+
 enum class state_t : uint8_t
 {
-    INIT = 0,
-    READY = 1,
+    INIT    = 0,
+    READY   = 1,
     PENDING = 2,
-    ERROR = 3,
+    ERROR   = 3,
 };
